d4c9d5c4cb1186cf413f32706d4269403f3d3ad6
chinoad <46451027+chinoad@users.noreply.github.com>
2019-02-13 16:33:17 -0500

    Update PhoneNumberApp.java
    Updated to take in multiple numbers but still needs some work 👎

--- src/PhoneNumApp/PhoneNumberApp.java
+++ src/PhoneNumApp/PhoneNumberApp.java
@@ -9,10 +9,14 @@ public class PhoneNumberApp {
 
         String filename = "/Users/chihiro/Desktop/JavaPrograms/PhoneNumApp/phonenumber.txt";
         File file = new File(filename);
+        String[] phoneNums = new String[4];
         String phoneNum = null;
+
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
-            phoneNum = br.readLine();
+            for (int i = 0; i < phoneNums.length; i++) {
+                phoneNums[i] = br.readLine();
+            }
             br.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error!! File not found!");
@@ -20,29 +24,65 @@ public class PhoneNumberApp {
             System.out.println("Error! Cannot read file");
         }
 
+        for (int i = 0; i < phoneNums.length; i++) {
+            phoneNum = phoneNums[i];
+            try {
+                if ((phoneNum == "911") || (phoneNum.length() != 10) || (phoneNum.substring(0, 1).equals("0"))) {
+                    throw new EmergencyException(phoneNum);
+                } else if (phoneNum.substring(0, 1).equals("0")) {
+                    throw new AreaCodeException(phoneNum);
+                } else if (phoneNum.length() != 10) ;
+                throw new TenDigitsException(phoneNum);
 
-        try {
-            if (phoneNum.length() != 10) {
-                throw new TenDigitsException();
+            } catch (EmergencyException e) {
+                System.out.println("Error! The number cannot be 911");
+                System.out.println(e.toString());
+
+            } catch (TenDigitsException e) {
+                System.out.println("Error! The number is not 10 digits");
+                System.out.println(e.toString());
+            } catch (AreaCodeException e) {
+                System.out.println("Error! The number cannot start with 0");
+                System.out.println(e.toString());
             }
-            System.out.println(phoneNum);
-        } catch(TenDigitsException e) {
-            System.out.println("Error! The number is not 10 digits");
+
         }
 
     }
-
-
 }
 
-class TenDigitsException extends Exception{
+class TenDigitsException extends Exception {
+    String num;
 
+    TenDigitsException(String num) {
+        this.num = num;
+    }
+
+    public String toString() {
+        return ("TenDigitsException: " + num);
+    }
 }
 
 class AreaCodeException extends Exception {
+    String num;
 
+    AreaCodeException(String num) {
+        this.num = num;
+    }
+
+    public String toString() {
+        return ("AreaCodeException: " + num);
+    }
 }
 
 class EmergencyException extends Exception {
+    String num;
 
+    EmergencyException(String num) {
+        this.num = num;
+    }
+
+    public String toString() {
+        return ("EmergencyException: " + num);
+    }
 }
